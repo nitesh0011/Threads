d5de7015e4de885e4cd67290dfb40a68a39f0918
nitesh0011 <patilnitesh590@gmail.com>
2024-03-24 20:48:36 +0530

    vercel commit

--- middleware.ts
+++ middleware.ts
@@ -2,22 +2,20 @@ import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { NextResponse } from "next/server";
  
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
+
+    
+
+
+
   afterAuth(auth, req) {
-    // Handle users who aren't authenticated
     if (!auth.userId && !auth.isPublicRoute) {
       return redirectToSignIn({ returnBackUrl: req.url });
     }
-    // Redirect logged in users to organization selection page if they are not active in an organization
    
-    // If the user is logged in and trying to access a protected route, allow them to access route
     if (auth.userId && !auth.isPublicRoute) {
       return NextResponse.next();
     }
-    // Allow users visiting public routes to access them
     return NextResponse.next();
   },
   publicRoutes:['/sign-in','/sign-up']
