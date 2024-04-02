0b44595ffa2eb58d0ca192ccbb4bd593308b92a7
nitesh0011 <patilnitesh590@gmail.com>
2024-04-02 21:40:55 +0530

    vercel commit

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -9,7 +9,7 @@ const Onboarding =async () => {
   const user = await currentUser();
   if (!user) return null;
   
-  const userInfo = await fetchUser(user.id);
+  const userInfo:any = await fetchUser(user.id);
   if (userInfo?.onboarded) redirect("/");
 
 
